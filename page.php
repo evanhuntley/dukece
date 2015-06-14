9168b2c97fb979e574fc9e414cbb4db3a1d2f3f1
Evan Huntley <evan.huntley@gmail.com>
2015-06-13 22:28:14 -0400

    First pass on general layout and page styles

--- page.php
+++ page.php
@@ -1,23 +1,34 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <div class="wrap">
 
-    <article role="main" class="primary-content type-page" id="post-<?php the_ID(); ?>">
-        <?php if ( is_front_page() ) { ?>
-            <h1><?php the_title(); ?></h1>
-        <?php } else { ?>
-            <h1><?php the_title(); ?></h1>
-        <?php } ?>
+        <div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
+            <?php if(function_exists('bcn_display'))
+            {
+                bcn_display();
+            }?>
+        </div>
 
-        <?php the_content(); ?>
+        <aside class="section-sidebar">
+            Sample stuff
+        </aside>
 
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:' ), 'after' => '</div>' ) ); ?>
+        <article role="main" class="primary-content type-page" id="post-<?php the_ID(); ?>">
+            <?php if ( is_front_page() ) { ?>
+                <h1><?php the_title(); ?></h1>
+            <?php } else { ?>
+                <h1><?php the_title(); ?></h1>
+            <?php } ?>
 
-        <?php edit_post_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
+            <?php the_content(); ?>
 
-        <?php endwhile; ?>
-    </article>
+            <?php edit_post_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
 
-    <?php get_sidebar(); ?>
+            <?php endwhile; ?>
+        </article>
+
+        <?php get_sidebar(); ?>
+    </div>
 
 <?php get_footer( 'no-sidebar' ); // will include footer-no-sidebar.php; ?>
