5c1698cfc9357ad5faf43dbc23f6ac4c288b4258
Evan Huntley <evan.huntley@gmail.com>
2015-09-08 08:30:44 -0400

    Minor tweaks for responsive

--- assets/scripts/partials/general.js
+++ assets/scripts/partials/general.js
@@ -23,6 +23,26 @@ jQuery(function($) {
         $(this).toggleClass('active');
     });
 
+    $('.menu-item-has-children').each(function(e) {
+        var self = $(this);
+        self.append('<span class="toggle-sub"></span>');
+    });
+
+    $('.toggle-sub').click(function(e) {
+        $(this).toggleClass('active');
+        var submenu = $(this).prev('.sub-menu');
+
+        if ( submenu.is(':visible')) {
+            submenu.slideToggle();
+        } else {
+            $('.sub-menu').slideUp();
+            submenu.slideToggle();
+        }
+
+
+        e.preventDefault();
+    });
+
     // Init Flexslider
     $('.flexslider').flexslider({
         controlNav: false,
