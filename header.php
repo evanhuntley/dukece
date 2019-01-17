770acc9426330c2c861285b25eb5f54472fc1169
Evan Huntley <evan.huntley@gmail.com>
2019-01-17 07:55:18 -0500

    Adding linkedin tracking

--- header.php
+++ header.php
@@ -107,6 +107,34 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 <!-- End Google Tag Manager -->
 
+<script type="text/javascript">
+
+_linkedin_partner_id = "639698";
+
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+</script><script type="text/javascript">
+
+(function(){var s = document.getElementsByTagName("script")[0];
+
+var b = document.createElement("script");
+
+b.type = "text/javascript";b.async = true;
+
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+
+s.parentNode.insertBefore(b, s);})();
+
+</script>
+
+<noscript>
+
+<img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=639698&fmt=gif" />
+
+</noscript>
+
 <!-- Vimeo for GA -->
 <!-- <script type="text/javascript" defer="defer" src="https://extend.vimeocdn.com/ga/11112294.js"></script> -->
 <script src="https://player.vimeo.com/api/player.js"></script>
